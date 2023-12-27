756f60faceefeee673b81630c68c2aa945d14d77
nazam9519 <nazam9519@gmail.com>
2023-12-27 18:18:31 -0500

    added dynamic variables for dir creation with docker compose

---
+++ setuser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export username="tarr"
+export groupname="tarrshares"
+export tarrid=1002
+export tarrhome="<tarr home dir>"
+export tarrdata="$tarrhome/<data dir>"
+export tarrconf="$tarrhome/<conf dir>"
+#sudo groupadd -g $tarrid $groupname
+#sudo useradd -m -d $tarrhome $tarrid -g $tarrid $username

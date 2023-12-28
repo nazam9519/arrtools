ad792b68c1d94ace6ddf88ce2ba64de51e13f706
nazam9519 <nazam9519@gmail.com>
2023-12-27 20:22:24 -0500

    added plex and updated setuser.sh

--- setuser.sh
+++ setuser.sh
@@ -2,8 +2,10 @@
 export username="tarr"
 export groupname="tarrshares"
 export tarrid=1002
-export tarrhome="<tarr home dir>"
-export tarrdata="$tarrhome/<data dir>"
-export tarrconf="$tarrhome/<conf dir>"
-#sudo groupadd -g $tarrid $groupname
-#sudo useradd -m -d $tarrhome $tarrid -g $tarrid $username
+export tarrhome="/home/tarr"
+export tarrdata="$tarrhome/docker/data"
+export tarrconf="$tarrhome/docker/appconfig"
+echo $tarrdata
+echo $tarrconf
+sudo groupadd -g $tarrid $groupname
+sudo useradd -m -d $tarrhome $tarrid -g $tarrid $username

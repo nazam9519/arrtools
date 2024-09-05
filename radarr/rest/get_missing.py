13b37c59975685bc32209d305bdf5f6285c7892c
Nabil A <nabila@Nabils-MacBook-Air.local>
2024-09-05 12:46:05 -0400

    cd /Users/nabila/projects/arr ; /usr/bin/env /usr/local/bin/python3 /Users/nabila/.vscode/extensions/ms-python.debugpy-2024.0.0-darwin-arm64/bundled/libs/debugpy/adapter/../../debugpy/launcher 51352 -- /Users/nabila/projects/arr/arrtools/arrtools/radarr/rest/get_missing.py reformat and remove unnecessary code in radarr get

--- radarr/rest/get_missing.py
+++ radarr/rest/get_missing.py
@@ -1,45 +1,73 @@
 import requests
 import json 
+from itertools import islice
 
+"""
+    This script goes out to radarr, grabs missing movies and searches for them again
+    Search can be limited with search with the specify_str variable(to be changed to argument parser)
+"""
 def main():
-    rest_url = "<your url>"
+    #setup url and rest calls
+    rest_url = "<your rest url>"
     rest_call = {
-        "missing":"api/v3/wanted/missing",
-        "movie":"api/v3/movie",
-        "command":"api/v3/command"
+        "missing":f"{constants['api']}/wanted/missing",
+        "movie":f"{constants['api']}/movie",
+        "command":f"{constants['api']}/command"
     }
-    missing = radarr_req(
-        path=f"{rest_url}/{rest_call['missing']}",
+
+    #get movies in missing status
+    missing = requests.get(
+        url=f"{rest_url}/{rest_call['missing']}",
         params=constants["radarrPageSize"],
-        method="GET"
+        headers=headers
     )
+    
+    #check if the movie was found
     if missing.status_code != 200:
         print(f"Error: [{missing.status_code}]-{missing.reason}")
         return
     else:
         missing = missing.json()
-    x = [i for i in missing['records'] if "Godzilla" in i['title'] in i['title']]
-    print(x)
-    for i in x:        
+
+    
+    #check for the movie filters
+    if specify_str is not None:
+        d = [i for i in missing['records'] if specify_str in i['title']]
+        for i in d:  
+             req = requests.post(
+                url=f"{rest_url}/{rest_call['command']}",
+                headers=headers,
+                json={"name":"MoviesSearch","movieIds":[i['id']]}
+            )
+        return
+    
+    #grab all missing movies
+    missing_movies = [i for i in missing['records']]
+    for index,i in enumerate(islice(missing_movies,10)):  
         req = requests.post(
             url=f"{rest_url}/{rest_call['command']}",
             headers=headers,
-            #params={"id":i['id']},
             json={"name":"MoviesSearch","movieIds":[i['id']]}
         )
-        print(req.status_code)
-        print(req.json())
-
-def radarr_req(path: str,method: str,params:dict={})->requests.Request:
-    return requests.request(
-        method=method,
-        url=path,
-        headers=headers,
-        params=params
-    )
 
 if __name__ == "__main__":
-    api_token = "your api token"
+    """CONSTANT GLOBALS
+       api_token from radarr -> settings -> general-> API Key
+       headers- uses api_token for auth
+    """
+    api_token = "<your radarr api token>"
     headers={"Authorization":f"Bearer {api_token}"}
-    constants = {"radarrPageSize":{"pageSize":100},"api":"/api/v3"}
+
+    """CONSTANTS
+       radarrpagesize- defines how many missing movies are returned per page
+                       pageSize is the actual parameter that radarr's rest api needs
+       api-
+    """
+    constants = {"radarrPageSize":{"pageSize":100},"api":"api/v3"}
+
+    """FILTER CONSTANTS
+    TODO change to using argparse instead since this will be a cron job
+       specify-str: this will filter out movies without this title 
+    """
+    specify_str = None
     main()

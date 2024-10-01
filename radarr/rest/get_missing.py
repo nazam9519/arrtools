35395a994eeab51dbd90f85d32f44d02bba7e9fe
Nabil A <nabila@macbil.nabil.net>
2024-10-01 09:04:04 -0400

    added argparse

--- radarr/rest/get_missing.py
+++ radarr/rest/get_missing.py
@@ -1,6 +1,7 @@
 import requests
 import json 
 from itertools import islice
+import argparse
 
 """
     This script goes out to radarr, grabs missing movies and searches for them again
@@ -8,10 +9,9 @@ from itertools import islice
 """
 def main():
     #setup url and rest calls
-    rest_url = "<your rest url>"
+    rest_url = "http://unraid.nabil.net:7878"
     rest_call = {
         "missing":f"{constants['api']}/wanted/missing",
-        "movie":f"{constants['api']}/movie",
         "command":f"{constants['api']}/command"
     }
 
@@ -30,7 +30,8 @@ def main():
         missing = missing.json()
 
     
-    #check for the movie filters
+    #check for the movie filters and execute a post to the command api
+    #this triggers the automatic search function for the movie in radarr
     if specify_str is not None:
         d = [i for i in missing['records'] if specify_str in i['title']]
         for i in d:  
@@ -40,34 +41,39 @@ def main():
                 json={"name":"MoviesSearch","movieIds":[i['id']]}
             )
         return
-    
-    #grab all missing movies
+    print("go time!")
+    #grab all missing movies and execute the post to the command api 
+    #this executes the automatic search function for the movie in radarr
     missing_movies = [i for i in missing['records']]
-    for index,i in enumerate(islice(missing_movies,10)):  
+    for index,i in enumerate(islice(missing_movies,len(missing_movies))):  
         req = requests.post(
             url=f"{rest_url}/{rest_call['command']}",
             headers=headers,
             json={"name":"MoviesSearch","movieIds":[i['id']]}
         )
-
+    with open('retried_movies.txt','w') as outfile:
+        json.dump(missing_movies,outfile,indent=4)
 if __name__ == "__main__":
     """CONSTANT GLOBALS
-       api_token from radarr -> settings -> general-> API Key
+       api_token- token from radarr -> settings -> general-> API Key
        headers- uses api_token for auth
     """
-    api_token = "<your radarr api token>"
+    api_token = "110b36b161b046c5b9a6551ee34479fd"
     headers={"Authorization":f"Bearer {api_token}"}
 
     """CONSTANTS
        radarrpagesize- defines how many missing movies are returned per page
                        pageSize is the actual parameter that radarr's rest api needs
-       api-
+       api- radarr's constant rest api path
     """
     constants = {"radarrPageSize":{"pageSize":100},"api":"api/v3"}
 
     """FILTER CONSTANTS
-    TODO change to using argparse instead since this will be a cron job
+       usage would be <exec> get_missing.py -f[--filter] 'optional movie filter' 
        specify-str: this will filter out movies without this title 
     """
-    specify_str = None
+    parser = argparse.ArgumentParser(description="A script to automate ur downloads")
+    parser.add_argument('-f','--filter',required=False,default=None)
+    args = parser.parse_args()
+    specify_str = args.filter
     main()
